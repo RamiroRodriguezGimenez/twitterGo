34c5afee95beaea25cbdee9d4fcddf01a95067f1
ramiro.a.rodriguez <ramirorodriguezgiemenez@gmail.com>
2024-12-13 12:18:35 -0300

    utlimo commit

--- subo.bat
+++ subo.bat
@@ -1,6 +1,6 @@
 git add .
 git commit -m "utlimo commit"
 git push
- go build -o bootstrap main.go
+$env:GOOS="linux"; $env:GOARCH="amd64"; go build -o bootstrap main.go
 del lambda-handler.zip
 tar.exe -a -cf lambda-handler.zip bootstrap
